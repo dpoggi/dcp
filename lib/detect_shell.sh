270ef24a974fb46241130bc9980fd9d5a803eea4
Dan Poggi <dan.poggi@gmail.com>
2017-06-13 20:16:20 -0400

    Fix shell/invocation detection

--- lib/detect_shell.sh
+++ lib/detect_shell.sh
@@ -1,10 +1,14 @@
 if [[ -n "${ZSH_NAME}" ]]; then
+  readonly DCP_SHELL="zsh"
+
   if [[ "$-" = *l* ]]; then
     readonly DCP_SHELL_INVOCATION="exec -l zsh -l"
   else
     readonly DCP_SHELL_INVOCATION="exec zsh"
   fi
 else
+  readonly DCP_SHELL="bash"
+
   if shopt -q login_shell 2> /dev/null; then
     readonly DCP_SHELL_INVOCATION="exec -l bash -l"
   else
