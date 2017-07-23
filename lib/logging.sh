2fc0b37338dc17bbf342b0bd9caf40cc2fef4a51
Dan Poggi <dan.poggi@gmail.com>
2017-07-22 21:48:38 -0400

    Fix logging, again

--- lib/logging.sh
+++ lib/logging.sh
@@ -1,14 +1,14 @@
-__logfln() {
+__logf() {
   local lvl="$1"; shift
   local lvl_clr="$1"; shift
   local fmt="$1"; shift
 
   if [[ -t 1 ]]; then
-    printf "\033[2;39;49m%s ${lvl_clr}${lvl}\033[2;39;49m : \033[0m${fmt}\n" \
+    printf "\033[2;39;49m%s ${lvl_clr}${lvl}\033[2;39;49m : \033[0m${fmt}" \
            "$(__log_date)" \
            "$@"
   else
-    printf "%s ${lvl} : ${fmt}\n" \
+    printf "%s ${lvl} : ${fmt}" \
            "$(__log_date)" \
            "$@"
   fi
@@ -16,6 +16,10 @@ __logfln() {
 
 __log_date() { date "+%Y-%m-%d %H:%M:%S"; }
 
-infofln() { __logfln " INFO" "\033[0;34m" "$@"; }
-warnfln() { __logfln " WARN" "\033[0;33m" "$@"; }
-errorfln() { __logfln "ERROR" "\033[0;31m" "$@"; }
+infof() { __logf " INFO" "\033[0;34m" "$@"; }
+warnf() { __logf " WARN" "\033[0;33m" "$@"; }
+errorf() { __logf "ERROR" "\033[0;31m" "$@"; }
+
+infofln() { infof "$@"; printf "\n"; }
+warnfln() { warnf "$@"; printf "\n"; }
+errorfln() { errorf "$@"; printf "\n"; }
