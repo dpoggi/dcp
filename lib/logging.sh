56844e6767251bb1d87671d972061191496317df
Dan Poggi <dan.poggi@gmail.com>
2017-12-08 18:47:33 -0500

    Logging with levels!

--- lib/logging.sh
+++ lib/logging.sh
@@ -1,25 +1,54 @@
-__logf() {
-  local lvl="$1"; shift
-  local lvl_clr="$1"; shift
-  local fmt="$1"; shift
+if [[ -t 1 ]]; then
+  __logf() {
+    local level="$1"; shift
+    local level_color="$1"; shift
+    local format="$1"; shift
+    printf "\033[2;39;49m%s\033[0m ${level_color}${level}\033[0;35;49m %s\033[2;39m : \033[0m${format}" \
+           "$(date '+%F %T')" "$$" "$@"
+  }
+else
+  __logf() {
+    local level="$1"; shift; shift
+    local format="$1"; shift
+    printf "%s ${level} %s : ${format}" \
+           "$(date '+%F %T')" "$$" "$@"
+  }
+fi
 
-  if [[ -t 1 ]]; then
-    printf "\033[2;39;49m%s ${lvl_clr}${lvl}\033[2;39;49m : \033[0m${fmt}" \
-           "$(__log_date)" \
-           "$@"
-  else
-    printf "%s ${lvl} : ${fmt}" \
-           "$(__log_date)" \
-           "$@"
-  fi
-}
+if [[ -z "${DCP_LOG_LEVEL}" ]]; then
+  DCP_LOG_LEVEL="INFO"
+fi
+readonly DCP_LOG_LEVEL
+export DCP_LOG_LEVEL
 
-__log_date() { date "+%Y-%m-%d %H:%M:%S"; }
+if [[ " DEBUG INFO WARN ERROR " != *" ${DCP_LOG_LEVEL} "* ]]; then
+  printf >&2 "Error: invalid DCP_LOG_LEVEL=\"%s\"\n" "${DCP_LOG_LEVEL}"
+  exit 1
+fi
 
-infof() { __logf " INFO" "\033[0;34m" "$@"; }
-warnf() { __logf " WARN" "\033[0;33m" "$@"; }
-errorf() { __logf "ERROR" "\033[0;31m" "$@"; }
+if [[ "${DCP_LOG_LEVEL}" = "DEBUG" ]]; then
+  debugf() { __logf "DEBUG" "\033[0;32m" "$@"; }
+  debugfln() { debugf "$@"; printf "\n"; }
+else
+  debugf() { :; }
+  debugfln() { :; }
+fi
 
-infofln() { infof "$@"; printf "\n"; }
-warnfln() { warnf "$@"; printf "\n"; }
+if [[ "${DCP_LOG_LEVEL}" = "DEBUG" || "${DCP_LOG_LEVEL}" = "INFO" ]]; then
+  infof() { __logf " INFO" "\033[0;34m" "$@"; }
+  infofln() { infof "$@"; printf "\n"; }
+else
+  infof() { :; }
+  infofln() { :; }
+fi
+
+if [[ "${DCP_LOG_LEVEL}" != "ERROR" ]]; then
+  warnf() { __logf " WARN" "\033[0;33m" "$@"; }
+  warnfln() { warnf "$@"; printf "\n"; }
+else
+  warnf() { :; }
+  warnfln() { :; }
+fi
+
+errorf() { __logf "ERROR" "\033[1;31m" "$@"; }
 errorfln() { errorf "$@"; printf "\n"; }
