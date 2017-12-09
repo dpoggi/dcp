6a818b2bb3d6fcf18fafa557e1af9fa60a56487e
Dan Poggi <dan.poggi@gmail.com>
2017-12-08 19:02:08 -0500

    Constants before functions

--- lib/logging.sh
+++ lib/logging.sh
@@ -1,9 +1,15 @@
+if [[ " DEBUG INFO WARN ERROR " != *" ${DCP_LOG_LEVEL} "* ]]; then
+  DCP_LOG_LEVEL="INFO"
+fi
+readonly DCP_LOG_LEVEL
+export DCP_LOG_LEVEL
+
 if [[ -t 1 ]]; then
   __logf() {
     local level="$1"; shift
     local level_color="$1"; shift
     local format="$1"; shift
-    printf "\033[2;39;49m%s\033[0m ${level_color}${level}\033[0;35;49m %s\033[2;39m : \033[0m${format}" \
+    printf "\033[2;39;49m%s\033[0m ${level_color}${level}\033[0;35;49m %s\033[2;39m :\033[0m ${format}" \
            "$(date '+%F %T')" "$$" "$@"
   }
 else
@@ -14,12 +20,6 @@ else
            "$(date '+%F %T')" "$$" "$@"
   }
 fi
-
-if [[ " DEBUG INFO WARN ERROR " != *" ${DCP_LOG_LEVEL} "* ]]; then
-  DCP_LOG_LEVEL="INFO"
-fi
-readonly DCP_LOG_LEVEL
-export DCP_LOG_LEVEL
 
 if [[ "${DCP_LOG_LEVEL}" = "DEBUG" ]]; then
   debugf() { __logf "DEBUG" "\033[0;32m" "$@"; }
