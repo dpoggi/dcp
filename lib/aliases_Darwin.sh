261e91f3addde02192f0a43f9e0ac7a348a3b6e0
Dan Poggi <dan.poggi@gmail.com>
2019-01-19 00:50:59 -0500

    Improvements to Darwin aliases

--- lib/aliases_Darwin.sh
+++ lib/aliases_Darwin.sh
@@ -4,7 +4,11 @@
 
 # Open Xcode for current folder (prefers workspace to project)
 xc() {
-  /usr/bin/open -a "$(__get_xcode_app)" "${1:-.}"
+  local xcode_app
+  if ! xcode_app="$(__get_xcode_app)"; then
+    xcode_app="Xcode"
+  fi
+  /usr/bin/open -a "${xcode_app}" "${1:-.}"
 }
 
 # Dammit Xcode (delete derived data twice a day for entire career as needed)
@@ -19,23 +23,24 @@ fuxcode() {
 # Dammit CoreSimulator (kill simulator service twice a day for entire career as needed)
 fucoresim() {
   __check_dt_not_running || return 1
-
   /bin/launchctl bootout "user/${UID}/com.apple.CoreSimulator.CoreSimulatorService"
-  return "$?"
 }
 
 # Dammit Interface Builder (ibtool)
 fuibtool() {
   __check_dt_not_running || return 1
-
   /usr/bin/pkill -KILL -x -U "${UID}" "ibtoold"
-  return "$?"
 }
 
 # Verify Xcode installation
 haxcode() {
-  /usr/sbin/spctl --assess --verbose "$(__get_xcode_app)"
-  return "$?"
+  local xcode_app
+  if ! xcode_app="$(__get_xcode_app)"; then
+    printf 'Error: Command Line Tools selected, or Xcode not installed.\n'
+    return 1
+  fi
+
+  /usr/sbin/spctl --assess --verbose "${xcode_app}"
 }
 
 __check_dt_not_running() {
@@ -49,29 +54,55 @@ __check_dt_not_running() {
   fi
 }
 
-# Get the path of the currently selected Xcode, or use the app name
-# if one cannot be found.
+# Get the path of the currently selected Xcode.
 __get_xcode_app() {
-  local developer_dir="$(/usr/bin/xcode-select --print-path 2>/dev/null)"
-  if [[ "${developer_dir}" != *".app/Contents/Developer" ]]; then
-    printf 'Xcode\n'; return
+  local developer_dir
+  if ! developer_dir="$(/usr/bin/xcode-select --print-path 2>/dev/null)"; then
+    return 1
   fi
-
-  local xcode_app="${developer_dir}"
-  while [[ "${xcode_app##*.}" != "app" ]]; do
-    xcode_app="$(dirname "${xcode_app}")"
-    if ((${#xcode_app} <= 1)); then
-      printf 'Xcode\n'; return
-    fi
-  done
-
-  printf '%s\n' "${xcode_app}"
+  if [[ "${developer_dir}" != *".app/Contents/Developer" ]]; then
+    return 1
+  fi
+  printf '%s\n' "${developer_dir%"/Contents/Developer"}"
 }
 
 #
 # Homebrew - fully update/upgrade, clean up the mess, optionally
 #            install something in between. Boop!
 #
+
+boop() {
+  __boop_check_pyenv
+
+  local err
+
+  brew update; err="$?"
+  ((err == 0)) || return "${err}"
+
+  brew upgrade; err="$?"
+  ((err == 0)) || return "${err}"
+
+  if (($# > 0)); then
+    brew install "$@"; err="$?"
+    ((err == 0)) || return "${err}"
+  fi
+
+  brew cleanup --prune=all -s
+}
+
+boop_cask() {
+  (($# > 0)) || return 1
+
+  local err
+
+  brew update; err="$?"
+  ((err == 0)) || return "${err}"
+
+  brew cask install "$@"; err="$?"
+  ((err == 0)) || return "${err}"
+
+  brew cleanup --prune=all -s
+}
 
 __boop_check_pyenv() {
   if ! __is_function mm_off; then
@@ -85,72 +116,45 @@ __boop_check_pyenv() {
 rbenv, rvm, pyenv, and/or nvm found in PATH. This can break installing or
 upgrading software from Homebrew. Run \`mm_off -a\` now to restart this
 EOT
-  printf >&2 "shell without it/them (y/n)? "
+  printf >&2 'shell without it/them (y/n)? '
 
   local confirmation
   read -r confirmation
   if [[ "${confirmation}" =~ ^[Yy] ]]; then
-    printf >&2 "\nRestarting the shell. Please run this command again.\n"
+    printf >&2 '\nRestarting the shell. Please run this command again.\n'
     mm_off -a
   fi
-}
-
-boop() {
-  __boop_check_pyenv
-
-  local result
-
-  brew update; result="$?"
-  [[ "${result}" = "0" ]] || return "${result}"
-
-  brew upgrade; result="$?"
-  [[ "${result}" = "0" ]] || return "${result}"
-
-  if (($# > 0)); then
-    brew install "$@"; result="$?"
-    [[ "${result}" = "0" ]] || return "${result}"
-  fi
-
-  brew cleanup --prune=all -s; result="$?"
-  [[ "${result}" = "0" ]] || return "${result}"
-}
-
-boop_cask() {
-  if (($# == 0)); then
-    return 1
-  fi
-
-  local result
-
-  brew update; result="$?"
-  [[ "${result}" = "0" ]] || return "${result}"
-
-  brew cask install "$@"; result="$?"
-  [[ "${result}" = "0" ]] || return "${result}"
-
-  brew cleanup --prune=all -s; result="$?"
-  [[ "${result}" = "0" ]] || return "${result}"
 }
 
 # Code signing helper for Homebrew binaries (notably GDB)
 
 if [[ -s "${DCP_CONFIG_DIR}/brew_codesign.sha1" ]]; then
   brew_codesign() {
-    if [[ ! "$(file -b "$1" 2>/dev/null)" =~ ^Mach-O\ .+\ (bundle|executable) ]] \
-       && ! [[ -d "$1" && -d "$1/Contents" ]]; then
-      printf >&2 "Error: argument is not a Mach-O bundle or executable file.\\n"
+    local base="$(basename "$1")"
+    if ! __is_macho_bundle "$1" && ! __is_macho_file "$1"; then
+      printf >&2 'Error: argument is not a Mach-O bundle or binary file.\n'
       return 1
     fi
 
-    local target_path="$1"
-    if [[ -h "${target_path}" ]]; then
-      target_path="$(cd "$(dirname "${target_path}")" && realpath -q "$(readlink -n "${target_path}")")"
+    local target_path
+    if command -v realpath >/dev/null; then
+      target_path="$(command realpath -q "$1")"
+    else
+      target_path="$1"
     fi
 
-    local fingerprint
-    fingerprint="$(<"${DCP_CONFIG_DIR}/brew_codesign.sha1")"
+    /usr/bin/xcrun --sdk macosx \
+      codesign --force --sign "$(<"${DCP_CONFIG_DIR}/brew_codesign.sha1")" "${target_path}"
+  }
 
-    xcrun codesign --force --sign "${fingerprint}" "${target_path}"
+  __is_macho_bundle() {
+    local bundle_name="$(basename "$1")"
+    bundle_name="${bundle_name%%.*}"
+    [[ -d "$1" ]] && { [[ -d "$1/Contents" ]] || __is_macho_file "$1/${bundle_name}"; }
+  }
+
+  __is_macho_file() {
+    [[ "$(/usr/bin/file -b "$1" 2>/dev/null)" = "Mach-O"* ]]
   }
 fi
 
@@ -160,29 +164,29 @@ fi
 
 copy_md5() {
   if [[ ! -f "$1" ]]; then
-    printf >&2 "Argument is not a file\n"
+    printf >&2 'Argument is not a file.\n'
     return 1
   fi
   
   local sum="$(/sbin/md5 -q "$1")"
-  printf "%s" "${sum}" | pbcopy
-  printf "%s\n" "${sum}"
-}
-
-__copy_sha() {
-  if [[ ! -f "$2" ]]; then
-    printf >&2 "Argument is not a file\n"
-    return 1
-  fi
-
-  local sum="$(/usr/bin/shasum -p -a "$1" "$2" | cut -d ' ' -f 1)"
-  printf "%s" "${sum}" | pbcopy
-  printf "%s\n" "${sum}"
+  printf '%s' "${sum}" | pbcopy
+  printf '%s\n' "${sum}"
 }
 
 copy_sha1() { __copy_sha 1 "$1"; }
 copy_sha256() { __copy_sha 256 "$1"; }
 copy_sha384() { __copy_sha 384 "$1"; }
+
+__copy_sha() {
+  if [[ ! -f "$2" ]]; then
+    printf >&2 'Argument is not a file.\n'
+    return 1
+  fi
+
+  local sum="$(/usr/bin/shasum -p -a "$1" "$2" | cut -d ' ' -f 1)"
+  printf '%s' "${sum}" | pbcopy
+  printf '%s\n' "${sum}"
+}
 
 # Fix obnoxious bug with macOS zsh completion for /usr/bin/du if coreutils is
 # installed via Homebrew.
@@ -208,7 +212,7 @@ reset_launch_services() {
 
 # Flush DNS cache and reset mDNSResponder
 reset_dns_cache() {
-  printf >&2 "Flushing the DNS cache (enter your user password if prompted)...\n"
+  printf >&2 'Flushing the DNS cache (enter your user password if prompted)...\n'
 
   if ! __is_file discoveryutil; then
     # Sane versions of macOS
@@ -222,10 +226,7 @@ reset_dns_cache() {
 }
 
 # Clear Quick Look's file locks (so you can empty the trash)
-reset_quick_look() {
-  qlmanage -r
-}
-
+reset_quick_look() { qlmanage -r; }
 
 #
 # Just launchd things (tm)
@@ -238,41 +239,45 @@ launchd_export() {
   launchctl setenv "$1" "$(__valueof "$1")"
 }
 
-# "action transformer" for protected services that can't be stopped traditionally
-__lctl_protected_action() {
-  case "$1" in
-    stop)     printf "unstoppable";;
-    restart)  printf "kickstart";;
-    *)        printf "%s" "$1"
-  esac
-}
-
 # launchctl wrapper for apsd (Apple Push Service: Messages.app, etc.)
 apsctl() {
-  "${DCP}/libexec/lctl.sh" apsctl \
-                           system /System/Library/LaunchDaemons/com.apple.apsd.plist \
-                           "$(__lctl_protected_action "$1")"
+  "${DCP}/libexec/lctl.sh" \
+    apsctl \
+    system /System/Library/LaunchDaemons/com.apple.apsd.plist \
+    "$(__lctl_protected_action "$1")"
 }
 
 # launchctl wrapper for CoreAudio (because sometimes there be dragons)
 coreaudioctl() {
-  "${DCP}/libexec/lctl.sh" coreaudioctl \
-                           system /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist \
-                           "$1"
+  "${DCP}/libexec/lctl.sh" \
+    coreaudioctl \
+    system /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist \
+    "$1"
 }
 
 # launchctl wrapper for bluetoothd
 bluetoothctl() {
-  "${DCP}/libexec/lctl.sh" bluetoothctl \
-                           system /System/Library/LaunchDaemons/com.apple.bluetoothd.plist \
-                           "$(__lctl_protected_action "$1")"
+  "${DCP}/libexec/lctl.sh" \
+    bluetoothctl \
+    system /System/Library/LaunchDaemons/com.apple.bluetoothd.plist \
+    "$(__lctl_protected_action "$1")"
 }
 
 # launchctl wrapper for cfprefsd agent (clear file locks / empty the trash)
 cfprefsctl() {
-  "${DCP}/libexec/lctl.sh" cfprefsctl \
-                           user /System/Library/LaunchAgents/com.apple.cfprefsd.xpc.agent.plist \
-                           "$(__lctl_protected_action "$1")"
+  "${DCP}/libexec/lctl.sh" \
+    cfprefsctl \
+    user /System/Library/LaunchAgents/com.apple.cfprefsd.xpc.agent.plist \
+    "$(__lctl_protected_action "$1")"
+}
+
+# "action transformer" for protected services that can't be stopped traditionally
+__lctl_protected_action() {
+  case "$1" in
+    stop)     printf 'unstoppable\n' ;;
+    restart)  printf 'kickstart\n' ;;
+    *)        printf '%s\n' "$1"
+  esac
 }
 
 # gpgconf wrapper for gpg-agent, if installed via Homebrew
@@ -280,15 +285,15 @@ cfprefsctl() {
 if [[ -h "/usr/local/opt/gnupg" ]]; then
   gpgagentctl() {
     if [[ "$1" = "stop" || "$1" = "restart" ]]; then
-      /usr/local/bin/gpgconf --kill gpg-agent
+      /usr/local/opt/gnupg/bin/gpgconf --kill "gpg-agent"
 
-      if [[ -n "$(ps -x | awk '$4 ~ /^gpg-agent/')" ]]; then
-        killall gpg-agent
+      if /usr/bin/pgrep -q -x -U "${UID}" "gpg-agent" 2>/dev/null; then
+        /usr/bin/pkill -KILL -x -U "${UID}" "gpg-agent"
       fi
     fi
 
     if [[ "$1" != "stop" ]]; then
-      /usr/local/bin/gpgconf --launch gpg-agent
+      /usr/local/opt/gnupg/bin/gpgconf --launch "gpg-agent"
     fi
   }
 fi
@@ -296,29 +301,20 @@ fi
 # `brew services' wrappers for chunkwm, khd, and kwm if installed via Homebrew
 
 if [[ -d "/usr/local/opt/chunkwm" ]]; then
-  chunkwmctl() {
-    if (($# == 0)); then
-      return 1
-    fi
-    brew services "$@" koekeishiya/formulae/chunkwm
-  }
+  chunkwmctl() { __brewctl koekeishiya/formulae/chunkwm "$@"; }
 fi
 if [[ -d "/usr/local/opt/khd" ]]; then
-  khdctl() {
-    if (($# == 0)); then
-      return 1
-    fi
-    brew services "$@" koekeishiya/formulae/khd
-  }
+  khdctl() { __brewctl koekeishiya/formulae/khd "$@"; }
 fi
 if [[ -d "/usr/local/opt/kwm" ]]; then
-  kwmctl() {
-    if (($# == 0)); then
-      return 1
-    fi
-    brew services "$@" koekeishiya/formulae/kwm
-  }
+  kwmctl() { __brewctl koekeishiya/formulae/kwm "$@"; }
 fi
+
+__brewctl() {
+  (($# > 1)) || return 1
+  local formula="$1"; shift
+  brew services "$@" "${formula}"
+}
 
 # htop(1) should generally be run as root on macOS
 if [[ -x "/usr/local/opt/htop/bin/htop" ]]; then
