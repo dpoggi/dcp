8245c3a035917ab6d990fe95155fbe6684b59884
Dan Poggi <dan.poggi@gmail.com>
2020-03-27 22:54:01 -0400

    Make logging.sh a nice, usable thing

--- lib/logging.sh
+++ lib/logging.sh
@@ -1,49 +1,77 @@
-if [[ " DEBUG INFO WARN ERROR " != *" ${DCP_LOG_LEVEL} "* ]]; then
-  DCP_LOG_LEVEL="INFO"
-fi
-readonly DCP_LOG_LEVEL
-export DCP_LOG_LEVEL
+__dcp_contains() {
+  local search="$1"; shift
+  while (($# > 0)); do
+    [[ "$1" = "${search}" ]] && return || shift
+  done
+  return 1
+}
 
-if [[ -t 1 ]]; then
-  __logf() {
+declare DCP_LOG_COLOR DCP_LOG_LEVEL >/dev/null
+
+if ! __dcp_contains "${DCP_LOG_COLOR}" always auto never; then
+  DCP_LOG_COLOR="auto"
+fi
+if ! __dcp_contains "${DCP_LOG_LEVEL}" debug info warn error hush none off quiet silent; then
+  DCP_LOG_LEVEL="info"
+fi
+
+declare -rx DCP_LOG_COLOR DCP_LOG_LEVEL
+
+if { printf '' >&3; } 2>/dev/null; then
+  __dcp_printf() { printf "$@" >&3; }
+else
+  __dcp_printf() { printf "$@" >&2; }
+fi
+
+if [[ "${DCP_LOG_COLOR}" = "always" ]] || [[ "${DCP_LOG_COLOR}" = "auto" && -t 2 ]]; then
+  __dcp_logf() {
     local level="$1"; shift
     local level_color="$1"; shift
     local format="$1"; shift
-    printf "\033[2;39;49m%s\033[0m ${level_color}${level}\033[0;35;49m %s\033[2;39m :\033[0m ${format}" \
-           "$(date '+%F %T')" "$$" "$@"
+
+    __dcp_printf "\\033[2;39;49m%s %b%s \\033[0;35m%s \\033[2;39m: \\033[0m${format}" \
+                 "$(date '+%F %T')" "${level_color}" "${level}" "$$" "$@"
   }
 else
-  __logf() {
+  __dcp_logf() {
     local level="$1"; shift; shift
     local format="$1"; shift
-    printf "%s ${level} %s : ${format}" \
-           "$(date '+%F %T')" "$$" "$@"
+
+    __dcp_printf "%s [%s] %s : ${format}" \
+                 "$(date '+%F %T')" "${level}" "$$" "$@"
   }
 fi
 
-if [[ "${DCP_LOG_LEVEL}" = "DEBUG" ]]; then
-  debugf() { __logf "DEBUG" "\033[0;32m" "$@"; }
-  debugfln() { debugf "$@"; printf "\n"; }
+if __dcp_contains "${DCP_LOG_LEVEL}" debug; then
+  debugf() { __dcp_logf "DEBUG" '\033[0;32m' "$@"; }
+  debugfln() { debugf "$@"; __dcp_printf '\n'; }
 else
   debugf() { :; }
   debugfln() { :; }
 fi
 
-if [[ "${DCP_LOG_LEVEL}" = "DEBUG" || "${DCP_LOG_LEVEL}" = "INFO" ]]; then
-  infof() { __logf " INFO" "\033[0;34m" "$@"; }
-  infofln() { infof "$@"; printf "\n"; }
+if __dcp_contains "${DCP_LOG_LEVEL}" debug info; then
+  infof() { __dcp_logf " INFO" '\033[0;34m' "$@"; }
+  infofln() { infof "$@"; __dcp_printf '\n'; }
 else
   infof() { :; }
   infofln() { :; }
 fi
 
-if [[ "${DCP_LOG_LEVEL}" != "ERROR" ]]; then
-  warnf() { __logf " WARN" "\033[0;33m" "$@"; }
-  warnfln() { warnf "$@"; printf "\n"; }
+if __dcp_contains "${DCP_LOG_LEVEL}" debug info warn; then
+  warnf() { __dcp_logf " WARN" '\033[0;33m' "$@"; }
+  warnfln() { warnf "$@"; __dcp_printf '\n'; }
 else
   warnf() { :; }
   warnfln() { :; }
 fi
 
-errorf() { __logf "ERROR" "\033[1;31m" "$@"; }
-errorfln() { errorf "$@"; printf "\n"; }
+if __dcp_contains "${DCP_LOG_LEVEL}" debug info warn error; then
+  errorf() { __dcp_logf "ERROR" '\033[0;31m' "$@"; }
+  errorfln() { errorf "$@"; __dcp_printf '\n'; }
+else
+  errorf() { :; }
+  errorfln() { :; }
+fi
+
+unset -f __dcp_contains
