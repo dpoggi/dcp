2b9ccdc5f55a6c28a13814f13fe637b473c25522
Dan Poggi <dan.poggi@gmail.com>
2020-03-28 00:28:46 -0400

    Add log_cmd

--- lib/logging.sh
+++ lib/logging.sh
@@ -32,6 +32,14 @@ if [[ "${DCP_LOG_COLOR}" = "always" ]] || [[ "${DCP_LOG_COLOR}" = "auto" && -t 2
     __dcp_printf "\\033[2;39;49m%s %b%s \\033[0;35m%s \\033[2;39m: \\033[0m${format}" \
                  "$(date '+%F %T')" "${level_color}" "${level}" "$$" "$@"
   }
+
+  __dcp_log_cmd() {
+    local first="$1"; shift
+
+    __dcp_printf '\033[2;39;49m+ \033[0;32m%s\033[0m' "${first}"
+    __dcp_printf '%s' "${@/#/ }"
+    __dcp_printf '\n'
+  }
 else
   __dcp_logf() {
     local level="$1"; shift; shift
@@ -39,6 +47,14 @@ else
 
     __dcp_printf "%s [%s] %s : ${format}" \
                  "$(date '+%F %T')" "${level}" "$$" "$@"
+  }
+
+  __dcp_log_cmd() {
+    local first="$1"; shift
+
+    __dcp_printf '+ %s' "${first}"
+    __dcp_printf '%s' "${@/#/ }"
+    __dcp_printf '\n'
   }
 fi
 
@@ -73,5 +89,20 @@ else
   errorf() { :; }
   errorfln() { :; }
 fi
+
+log_cmd() {
+  (($# > 0)) || return
+
+  local arg cmd=() quote="'\\''"
+  for arg in "$@"; do
+    [[ "${arg}" =~ [!{}\`\'\"\ \\] ]] &&
+      cmd+=("'${arg//\'/${quote}}'") ||
+      cmd+=("${arg}")
+  done
+
+  __dcp_log_cmd "${cmd[@]}"
+
+  "$@"
+}
 
 unset -f __dcp_contains
