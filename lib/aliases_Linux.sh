5583014dbc9ed0479d73422ce2ac23f597845d9b
Dan Poggi <dan.poggi@gmail.com>
2021-02-15 19:28:04 -0500

    Improved netstat aliases

--- lib/aliases_Linux.sh
+++ lib/aliases_Linux.sh
@@ -3,31 +3,28 @@
 #
 
 __net_open_things() {
-  if ! __is_command netstat; then
-    printf >&2 "netstat is not installed!\n"
+  if ! command -v netstat >/dev/null; then
+    printf 'netstat not found\n' >&2
     return 1
   fi
 
-  printf "%s\n%s\n" "$1" \
-    "$(sudo -H netstat --all --listening --program --numeric --wide \
-           | awk "$2" | sort -g)" \
-         | column -t -o "    " \
-         | sed -r -e 's/^([0-9]+)(\s+)(\w+):(\w+) (.*)$/\1\2\3: \4\5/'
+  printf '%s\n%s\n' "$1" "$(sudo -H netstat -planW | awk "$2" | sort -g)" \
+    | column -t -o '    ' \
+    | sed -r -e 's/^([0-9]+)(\s+)(\w+):(\w+) (.*)$/\1\2\3: \4\5/'
 }
 
 # List open TCP/UDP ports
 
 net_open_ports() {
   sudo -H true
-  printf "======== Open TCP/UDP Ports ========\n"
+
+  printf '======== Open TCP/UDP Ports ========\n'
+
   __net_open_things "PID ProgramName Protocol BoundAddress" '{
-    if ($1 ~ /(tcp)6?/ && $6 == "LISTEN")
-    {
+    if ($1 ~ /(tcp)6?/ && $6 == "LISTEN") {
       sub("/", " ", $7)
       print $7 $8, $1, $4
-    }
-    else if ($1 ~ /(udp)6?/ && $6 != "ESTABLISHED")
-    {
+    } else if ($1 ~ /(udp)6?/ && $6 != "ESTABLISHED") {
       sub("/", " ", $6)
       print $6, $1, $4
     }
@@ -38,7 +35,9 @@ net_open_ports() {
 
 net_open_sockets() {
   sudo -H true
-  printf "======== Open Unix Sockets ========\n"
+
+  printf '======== Open Unix Sockets ========\n'
+
   __net_open_things "PID ProgramName Path" '{
     if ($1 == "unix" && $7 == "LISTENING") {
       sub("/", " ", $9)
@@ -51,6 +50,6 @@ net_open_sockets() {
 
 net_open_all() {
   net_open_ports
-  printf "\n"
+  printf '\n'
   net_open_sockets
 }
